63cd2d977e915a35371bcd69bb86984d326e625b
AnNgoIT <ngothuaan2002@gmail.com>
2023-05-15 03:50:15 +0700

    setting user profile

--- app/src/main/java/ute/fit/noithatapp/Model/UserModel.java
+++ app/src/main/java/ute/fit/noithatapp/Model/UserModel.java
@@ -7,6 +7,14 @@ public class UserModel {
     private String username;
     private String password;
 
+    private String image;
+
+    private String address;
+
+    public UserModel(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
 
     public UserModel(String name, String username, String password) {
         this.name = name;
@@ -14,47 +22,61 @@ public class UserModel {
         this.password = password;
     }
 
-    public UserModel(int id, String name, String username, String password) {
+    public UserModel(int id, String name, String username, String password, String image, String address) {
         this.id = id;
         this.name = name;
         this.username = username;
         this.password = password;
-    }
-    public UserModel(String username, String password) {
-        this.username = username;
-        this.password = password;
-    }
-
-    public void setId(int userId) {
-        this.id = userId;
+        this.image = image;
+        this.address = address;
     }
 
     public int getId() {
         return id;
     }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
     public String getName() {
         return name;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public String getPassword() {
-        return password;
     }
 
     public void setName(String name) {
         this.name = name;
     }
 
+    public String getUsername() {
+        return username;
+    }
+
     public void setUsername(String username) {
         this.username = username;
     }
 
+    public String getPassword() {
+        return password;
+    }
+
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
     }
 }
 
