39d73b3974edc30c11c22f1623976c1ca4d6deec
AnNgoIT <ngothuaan2002@gmail.com>
2023-05-18 02:39:59 +0700

    add api manager

--- src/main/java/com/example/demo/Controller/ManagerController.java
+++ src/main/java/com/example/demo/Controller/ManagerController.java
@@ -1,14 +1,20 @@
 package com.example.demo.Controller;
 
 
+import com.example.demo.DAO.CategoryDAO;
 import com.example.demo.DAO.OrderDAO;
 import com.example.demo.DAO.ProductDAO;
+import com.example.demo.Entity.Category;
 import com.example.demo.Entity.Order;
 import com.example.demo.Entity.Product;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.Optional;
 
 @RestController
@@ -18,6 +24,19 @@ public class ManagerController {
 
     @Autowired
     OrderDAO orderDAO;
+
+    @Autowired
+    private CategoryDAO categoryDAO;
+
+    @GetMapping("manager/countProductByCategoryId/{categoryId}")
+    public int getCountProductByCategoryId(@PathVariable("categoryId") Integer categoryId)
+    {
+        Optional<Category> category = categoryDAO.getCategoryById(categoryId);
+        ArrayList<Product> products = (ArrayList<Product>) productDAO.getProductByCategory(category);
+        int count = products.size();
+        return count;
+    }
+
     @PostMapping("manager/updateQuantityProduct/processed/{productId}")
     public void updateCountProduct(@PathVariable("productId") Integer productId, @RequestParam("quantity") Long quantity){
         Optional<Product> product=productDAO.getProductById(productId);
@@ -35,11 +54,41 @@ public class ManagerController {
     }
     @GetMapping("manager/revenue")
     public Long getTotalRevenue(){
-        ArrayList<Order> orderList = orderDAO.getOrdersByState("processing");
+        ArrayList<Order> orderList = orderDAO.getOrdersByState("processed");
         Long totalRevenue = Long.valueOf(0);
         for(Order order : orderList){
             totalRevenue += order.getProduct().getPrice() * order.getCount();
         }
         return totalRevenue;
     }
+    @GetMapping("manager/totalProcessed")
+    public int getTotalProcessed(){
+        ArrayList<Order> orderArrayList=orderDAO.getOrdersByState("processed");
+        return orderArrayList.size();
+    }
+    @GetMapping("manager/totalProcessing")
+    public int getTotalProcessing(){
+        ArrayList<Order> orderArrayList=orderDAO.getOrdersByState("processing");
+        return orderArrayList.size();
+    }
+
+    @GetMapping("manager/getTotalDay")
+    public Long getTotalDay(@RequestParam("day") String day){
+        ArrayList<Order> orderList = orderDAO.getOrdersByState("processed");
+        Long total =Long.valueOf(0);
+        for(Order order:orderList){
+            if (!(order.getDate() == null)){
+                Date input = order.getDate();
+                LocalDate date = input.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+                DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd");
+                String formattedDate = date.format(dateTimeFormatter);
+                if(formattedDate.equals(day)){
+                    total += order.getCount()*order.getProduct().getPrice();
+                }
+            }
+
+        }
+        return total;
+    }
+
 }
