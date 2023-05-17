5bc9b7f718e91dd9e54fe1d64d5e2182d50f53ae
AnNgoIT <ngothuaan2002@gmail.com>
2023-05-17 19:52:06 +0700

    pull the latest

--- app/src/main/java/ute/fit/noithatapp/Contants/Const.java
+++ app/src/main/java/ute/fit/noithatapp/Contants/Const.java
@@ -1,11 +1,10 @@
 package ute.fit.noithatapp.Contants;
 
 public class Const {
-<<<<<<< HEAD
-    public static String localhost = "172.30.144.1:3000";
-=======
-    public static String localhost = "172.20.160.1:3000";
->>>>>>> 07d715de6354067151f9fc02a63bccb2dfb8b4cf
+
+
+    public static String localhost = "192.168.2.26:3000";
+
     public static final String ROOT_URL = "http://" + localhost;
     public static final String URL_CATEGORY= ROOT_URL + "/user/categories/";
 }
